494c6df918a0fdd3c82b615a520937c498049a69
Ayanabha Misra <misrarimbo@gmail.com>
2023-12-02 23:46:10 +0530

    Added ansible

---
+++ ansible/roles/docker/files/install_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+sudo apt install -y docker.io
+sudo snap install docker
+
+sudo apt-get install -y docker-compose
---
+++ ansible/roles/docker/install_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo apt install docker.io
+sudo snap install docker
